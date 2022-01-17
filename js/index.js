878217fd4adc71ecb69020e2949b525d76bcf3ec
Vitoria Borba <vitoriaborba@icloud.com>
2022-01-17 19:30:09 +0000

    Interation3

--- js/index.js
+++ js/index.js
@@ -13,3 +13,21 @@ if (hacker1.length > hacker2.length) {
   }
 
 // Iteration 3: Loops
+// 3.1
+let driverName = ''
+for (letter of hacker1) {
+  driverName += letter.toUpperCase() + " "
+}
+// 3.2
+let navigatorName = ""
+for (i = hacker2.length -1; i >= 0; i-- ) {
+  navigatorName += hacker2[i];
+}
+//3.3
+if (hacker1.localeCompare(hacker2) === 1) { 
+    console.log("The driver's name goes first.");
+      } else if (hacker1.localeCompare(hacker2) === -1) {
+      console.log("Yo, the navigator goes first definitely.");
+    } else if (hacker1.localeCompare(hacker2)=== 0) {
+      console.log ("What?! You both have the same name?")
+    }
